9529ab05506669ed92b31b10efe17501186afb7e
muzgash <muzgash@gmail.com>
2022-04-02 23:39:51 -0500

    added subjects option to compendium endpoint

--- sds/plugins/CompendiumApp.py
+++ sds/plugins/CompendiumApp.py
@@ -13,12 +13,66 @@ class CompendiumApp(sdsPluginBase):
         super().__init__(sds)
 
 
-    def get_topics(self):
+    def get_subjects(self,page=1,max_results=10,institutions=[],groups=[],sort="citations",direction="descending"):
+        if not page:
+            page=1
+        else:
+            try:
+                page=int(page)
+            except:
+                print("Could not convert end page to int")
+                return None
+        if not max_results:
+            max_results=100
+        else:
+            try:
+                max_results=int(max_results)
+            except:
+                print("Could not convert end max to int")
+                return None
 
-        return {"data":""}
+        search_dict={}
+        var_dict={"name":1,"products_count":1,"citations_count":1,"products_by_year":1,"affiliations":1,"authors":1}
+        total=self.colav_db["subjects"].count_documents(search_dict)
+        cursor=self.colav_db["subjects"].find(search_dict,var_dict)
+        
+        if sort=="citations" and direction=="ascending":
+            cursor.sort([("citations_count",ASCENDING)])
+        if sort=="citations" and direction=="descending":
+            cursor.sort([("citations_count",DESCENDING)])
+        if sort=="production" and direction=="ascending":
+            cursor.sort([("products_count",ASCENDING)])
+        if sort=="production" and direction=="descending":
+            cursor.sort([("products_count",DESCENDING)])
+
+        skip = (max_results*(page-1))
+
+        cursor=cursor.skip(skip).limit(max_results)
+
+        data=[]
+        index=max_results*(page-1)
+        for subject in cursor:
+            index+=1
+            entry={
+                "index":index,
+                "name":subject["name"],
+                "id":subject["_id"],
+                "institutions":[], 
+                "authors":[], 
+                "groups":[], 
+                "plot":[] #citations and products per year
+            }
+            if "affiliations" in subject.keys():
+                entry["institutions"]=[{"name":aff["name"],"id":aff["id"]} for aff in subject["affiliations"] if not "type" in subject.keys()][:5]
+                entry["groups"]=[{"name":aff["name"],"id":aff["id"]} for aff in subject["affiliations"] if "type" in subject.keys()][:5]
+            if "authors" in subject.keys():
+                entry["authors"]=[{"name":au["name"],"id":au["id"]} for au in subject["authors"]][:5]
+            data.append(entry)
+
+        return {"data":data,"page":page,"count":max_results,"total":total}
 
 
-    def get_groups(self,page=1,max_results=100,sort="citations",direction="descending"):
+    def get_groups(self,page=1,max_results=10,groups="",institutions="",sort="citations",direction="descending"):
         if not page:
             page=1
         else:
@@ -37,6 +91,16 @@ class CompendiumApp(sdsPluginBase):
                 return None
 
         search_dict={"type":"group"}
+        in_list=[]
+        if groups:
+            in_list.extend(groups.split())
+        if institutions:
+            in_list.extend(institutions.split())
+        if len(in_list)>0:
+            def_list=[]
+            for iid in in_list:
+                def_list.append(ObjectId(iid))
+            search_dict["_id"]={"$in":def_list}
         var_dict={"name":1,"relations":1,"products_count":1,"citations_count":1,"products_by_year":1,"subjects":1}
         total=self.colav_db["affiliations"].count_documents(search_dict)
         cursor=self.colav_db["affiliations"].find(search_dict,var_dict)
@@ -106,7 +170,7 @@ class CompendiumApp(sdsPluginBase):
         return {"data":""}
 
 
-    def get_institutions(self,page=1,max_results=10,sort="citations",direction="descending"):
+    def get_institutions(self,page=1,max_results=10,groups="",institutions="",sort="citations",direction="descending"):
         if not page:
             page=1
         else:
@@ -125,6 +189,16 @@ class CompendiumApp(sdsPluginBase):
                 return None
 
         search_dict={}
+        in_list=[]
+        if groups:
+            in_list.extend(groups.split())
+        if institutions:
+            in_list.extend(institutions.split())
+        if len(in_list)>0:
+            def_list=[]
+            for iid in in_list:
+                def_list.append(ObjectId(iid))
+            search_dict["_id"]={"$in":def_list}
         var_dict={"name":1,"products_count":1,"citations_count":1,"products_by_year":1,"subjects":1}
         total=self.colav_db["affiliations"].count_documents(search_dict)
         cursor=self.colav_db["affiliations"].find(search_dict,)
@@ -213,6 +287,23 @@ class CompendiumApp(sdsPluginBase):
             if institutions:    
                 response = self.app.response_class(
                 response=self.json.dumps(institutions),
+                status=200,
+                mimetype='application/json'
+                )
+            else:
+                response = self.app.response_class(
+                response=self.json.dumps({"status":"Request returned empty"}),
+                status=204,
+                mimetype='application/json'
+            )
+        elif data=="subjects":
+            max_results=self.request.args.get('max')
+            page=self.request.args.get('page')
+            sort=self.request.args.get('sort')
+            subjects=self.get_subjects(page=page,max_results=max_results,sort=sort)
+            if subjects:    
+                response = self.app.response_class(
+                response=self.json.dumps(subjects),
                 status=200,
                 mimetype='application/json'
                 )
