0a2a4822b8dbc4f083559aed2635aa56aaa833ae
muzgash <muzgash@gmail.com>
2022-04-21 22:12:45 -0500

    Fixed all options for author endpoint according to the new standard

--- sds/plugins/AuthorsApp.py
+++ sds/plugins/AuthorsApp.py
@@ -257,22 +257,22 @@ class AuthorsApp(sdsPluginBase):
 
         data=[]
         names=[]
-        for key,val in result["subjects_by_year"].items():
-            year=int(key)
+        for val in result["subjects_by_year"]:
+            year=val["year"]
             if start_year:
                 if start_year>year:
                     continue
             if end_year:
                 if end_year<year:
                     continue
-            for sub in val:
+            for sub in val["subjects"]:
                 if sub["name"] in names:
-                    data[names.index(sub["name"])]["value"]+=sub["value"]
+                    data[names.index(sub["name"])]["products"]+=sub["products"]
                 else:
                     data.append(sub)
                     names.append(sub["name"])
         
-        sorted_data=sorted(data,key=lambda x:x["value"],reverse=True)
+        sorted_data=sorted(data,key=lambda x:x["products"],reverse=True)
                 
         return {"data":sorted_data[:limit],"total":len(data)}
 
@@ -329,21 +329,29 @@ class AuthorsApp(sdsPluginBase):
         }
 
         for reg in self.colav_db["documents"].aggregate(pipeline):
+            affiliation_id = ""
+            affiliation_name = ""
+            group_id=""
+            group_name=""
             if str(reg["_id"])==str(idx):
                 print("Skipped ",idx)
                 continue
             if "affiliations" in reg["author"].keys():
-                affiliation_id = reg["author"]["affiliations"][-1]["id"]
-                affiliation_name = reg["author"]["affiliations"][-1]["name"]
-
-            else: 
-                affiliation_id = ""
-                affiliation_name = ""
+                for aff in reg["author"]["affiliations"]:
+                    if "type" in aff.keys():
+                        if aff["type"]=="group":
+                            group_id=aff["id"]
+                            group_name=aff["name"]
+                    else:
+                        affiliation_id = aff["id"]
+                        affiliation_name = aff["name"]
 
             entry["coauthors"].append(
                 {"id":reg["_id"],"name":reg["author"]["full_name"],
-                "affiliation":{"institution":{"id":affiliation_id,
-                    "name":affiliation_name} },
+                "affiliation":{
+                    "institution":{"id":affiliation_id,"name":affiliation_name} ,
+                    "group":{"id":group_id,"name":group_name} ,
+                    },
                 "count":reg["count"]} 
             )
 
@@ -813,19 +821,13 @@ class AuthorsApp(sdsPluginBase):
         for doc in cursor:
             authors=[]
             for author in doc["authors"]:
-                au_entry={}
-                author_db=self.colav_db["authors"].find_one({"_id":author["id"]})
-                if author_db:
-                    au_entry={"full_name":author_db["full_name"],"id":author_db["_id"]}
-                affiliations=[]
+                au_entry={"full_name":author["full_name"],"id":author["id"],"affiliation":{}}
                 for aff in author["affiliations"]:
-                    aff_entry={}
-                    aff_db=self.colav_db["affiliations"].find_one({"_id":aff["id"]})
-                    if aff_db:
-                        aff_entry={"name":aff_db["name"],"id":aff_db["_id"]}
-                    
-                    affiliations.append(aff_entry)
-                au_entry["affiliations"]=affiliations
+                    if "type" in aff.keys():
+                        if aff["type"]=="group":
+                            au_entry["affiliation"]["group"]={"name":aff["name"],"id":aff["id"]}        
+                    else:
+                        au_entry["affiliation"]["institution"]={"name":aff["name"],"id":aff["id"]}
                 authors.append(au_entry)
 
             try:
