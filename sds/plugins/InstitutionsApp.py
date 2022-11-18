7b4d8eb566f1fcbc00a658451b13a04d6ba4347c
muzgash <muzgash@gmail.com>
2022-11-18 15:23:56 -0500

    Fixed sources and improve time by avoiding a request

--- sds/plugins/InstitutionsApp.py
+++ sds/plugins/InstitutionsApp.py
@@ -626,9 +626,7 @@ class InstitutionsApp(sdsPluginBase):
                         break
 
                 if "source" in paper.keys():
-                    source=self.colav_db["sources"].find_one({"_id":paper["source"]["id"]})
-                    if source:
-                        entry["source"]={"name":paper["source"]["title"],"id":paper["source"]["id"]}
+                    entry["source"]={"name":paper["source"]["title"],"id":paper["source"]["id"]}
                 
                 authors=[]
                 for author in paper["authors"]:
